2577273abdbcf3b8697b7dc66451afdd7fc75ebc
giri929 <reddygirish929@gmail.com>
2018-03-11 11:59:54 +0530

    Start Design

---
+++ Assets/Scripts/lightSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightSwitch : MonoBehaviour {
+
+	public bool isOn = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void change(){
+	
+		if (isOn) {
+			isOn = false;
+			this.transform.localEulerAngles = new Vector3 (0, 45, 0);
+		} else {
+			isOn = true;
+			this.transform.localEulerAngles = Vector3.zero;
+		}
+	}
+}
---
+++ Assets/Scripts/turnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int count = 0;
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				GameObject tmpGb = Instantiate (Resources.Load ("Cube", typeof(GameObject))) as GameObject;
+				tmpGb.transform.position = new Vector3 (j * 1.5f - 3f, i * -1.5f + 3f, 0);
+				tmpGb.name = count.ToString ();
+				count++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
